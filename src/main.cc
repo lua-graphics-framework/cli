ba5e12e66a3c3d323c2ef62f340f5100f2c1e80e
srevrtt <everettsistrunk@icloud.com>
2022-11-28 14:17:01 -0700

    Code cleanup

--- src/main.cc
+++ src/main.cc
@@ -5,18 +5,19 @@
 
 #ifdef _WIN32
 #include <Windows.h>
-#endif
-
 #include "include/help.h"
 #include "include/new.h"
 #include "include/update.h"
 #include "include/run.h"
 #include "include/install.h"
+#endif
 
 int main(int argc, char *argv[])
 {
+  // Make sure the user inputted an argument
   if (argc > 1)
   {
+    // Get all of the arguments
     std::vector<std::string> args;
     
     for (int i = 1; i < argc; i++)
@@ -24,10 +25,13 @@ int main(int argc, char *argv[])
       args.push_back(argv[i]);
     }
     
+    // Loop through each argument
     for (auto &str : args)
     {
+      // If we want to specify a parameter
       if (str.find("--", 0) == 0)
       {
+        // Help
         if (str == "--help")
         {
           showHelp();
@@ -38,23 +42,28 @@ int main(int argc, char *argv[])
           exit(1);
         }
       }
+      // If not, we want to input some options
       else
       {
+        // Creates a new LGF project
         if (str == "new")
         {
           newCmd();
         }
 
+        // Runs the LGF project
         if (str == "run")
         {
           runCmd();  
         }
         
+        // Updates LGF
         if (str == "update")
         {
           update();
         }
 
+        // Installs LGF
         if (str == "install")
         {
           installCmd();
